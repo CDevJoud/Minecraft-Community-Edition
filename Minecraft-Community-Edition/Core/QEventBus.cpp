5f47db79dab732f3cb62f574e494e5d51852e88e
BendedWills <bendedwills@gmail.com>
2026-03-05 23:50:20 -0600

    I was right the first time lol
    but release is still necessary...
    multithreading is hard

--- Minecraft-Community-Edition/Core/QEventBus.cpp
+++ Minecraft-Community-Edition/Core/QEventBus.cpp
@@ -17,12 +17,14 @@ namespace mce {
 	void QEventBus::runAsync() {
 		QEventBus::bIsRunning = true;
 		QEventBus::thread = std::thread([this] {
-			while (QEventBus::bIsRunning.load(std::memory_order_acquire)) {
+			// std::memory_order_relaxed is okay here, because order is irrelevant
+			while (QEventBus::bIsRunning.load(std::memory_order_relaxed)) {
 				eastl::unique_ptr<IEvent> event;
 				{
+					// Again, relaxed is okay, since the mutex ensures correct memory order.
 					std::unique_lock<std::mutex> lock(QEventBus::queueMutex);
-					QEventBus::conditionalVariable.wait(lock, [&]() { return !QEventBus::queue.empty() || !QEventBus::bIsRunning.load(std::memory_order_acquire); });
-					if (!QEventBus::bIsRunning.load(std::memory_order_acquire)) break;
+					QEventBus::conditionalVariable.wait(lock, [&]() { return !QEventBus::queue.empty() || !QEventBus::bIsRunning.load(std::memory_order_relaxed); });
+					if (!QEventBus::bIsRunning.load(std::memory_order_relaxed)) break;
 					event = std::move(QEventBus::queue.front());
 					QEventBus::queue.pop();
 				}
