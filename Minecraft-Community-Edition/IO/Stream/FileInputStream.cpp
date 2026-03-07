4cc532f5c9de9c8d18c271f274923e9944ce5f29
BendedWills <bendedwills@gmail.com>
2026-03-07 02:13:18 -0600

    Basic definitions

---
+++ Minecraft-Community-Edition/IO/Stream/FileInputStream.cpp
@@ -0,0 +1,15 @@
+#include "FileInputStream.hpp"
+
+namespace mce {
+	size_t FileInputStream::read(void* buffer, size_t size) {
+	}
+
+	size_t FileInputStream::seek(size_t position) {
+	}
+
+	size_t FileInputStream::tell() {
+	}
+
+	size_t FileInputStream::getSize() {
+	}
+}
---
+++ Minecraft-Community-Edition/IO/Stream/FileOutputStream.cpp
@@ -0,0 +1,18 @@
+#include "FileOutputStream.hpp"
+
+namespace mce {
+	void FileOutputStream::flush() {
+	}
+
+	size_t FileOutputStream::write(void* buffer, size_t size) {
+	}
+
+	size_t FileOutputStream::seek(size_t position) {
+	}
+
+	size_t FileOutputStream::tell() {
+	}
+
+	size_t FileOutputStream::getSize() {
+	}
+}
