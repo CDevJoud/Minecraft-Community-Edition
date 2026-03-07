de2cb90b3bbf6c44ecf5c58e9ab3fabaecc84392
BendedWills <bendedwills@gmail.com>
2026-03-07 00:59:59 -0600

    Add tests

--- Minecraft-Community-Edition/IO/InputStream.hpp
+++ Minecraft-Community-Edition/IO/InputStream.hpp
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <cstdint>
+
 namespace mce {
 	class InputStream {
 	public:
 		virtual ~InputStream() = 0;
+
+		virtual size_t read(void* buffer, size_t size) = 0;
+		virtual size_t seek(size_t position) = 0;
+		virtual size_t tell() = 0;
+		virtual size_t getSize() = 0;
 	};
 }
--- Minecraft-Community-Edition/IO/OutputStream.hpp
+++ Minecraft-Community-Edition/IO/OutputStream.hpp
@@ -1,8 +1,16 @@
 #pragma once
 
+#include <cstdint>
+
 namespace mce {
 	class OutputStream {
 	public:
 		virtual ~OutputStream() = 0;
+
+		virtual void flush() = 0;
+		virtual size_t write(void* buffer, size_t size) = 0;
+		virtual size_t seek(size_t position) = 0;
+		virtual size_t tell() = 0;
+		virtual size_t getSize() = 0;
 	};
 }
