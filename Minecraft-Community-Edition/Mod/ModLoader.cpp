6183dcf40e077ac1200f2a8bdc6a443c05aaca7f
Joud Kandeel <joudkandeel05@gmail.com>
2026-05-05 03:49:52 +0200

    modified the mod loader for now

--- Minecraft-Community-Edition/Mod/ModLoader.cpp
+++ Minecraft-Community-Edition/Mod/ModLoader.cpp
@@ -2,6 +2,7 @@
 
 static XIDevice* g_device;
 static XIContext* g_ctx;
+static XIExports* g_exp;
 eastl::unordered_map<Xuint64, Xvoid*> registeries;
 Xuint64 hash_str(Xcstr str) {
 	Xuint64 h = 0xcbf29ce484222325ULL; // FNV offset basis
@@ -72,6 +73,9 @@ Xvoid mce_ctx_release(XIContext* device) {
 	}
 }
 
+Xvoid mce_ctx_setXIExports(XIExports* exp) {
+	g_exp = exp;
+}
 
 Xuint32 mce_ctx_postEvent(XHQEventBus* qBus, Xvoid* event, Xconst Xuint64 type) {
 	auto it = registeries.find(qBus->idx);
@@ -136,6 +140,7 @@ Xint32 mce_createDeviceAndContext(XIDevice** device, XIContext** ctx) {
 
 	const_cast<XIContextVTable*>(ctx_vtbl)->addRef = mce_ctx_addRef;
 	const_cast<XIContextVTable*>(ctx_vtbl)->release = mce_ctx_release;
+	const_cast<XIContextVTable*>(ctx_vtbl)->setXIExports = mce_ctx_setXIExports;
 	const_cast<XIContextVTable*>(ctx_vtbl)->postEvent = mce_ctx_postEvent;
 
 	(*device)->vtbl = device_vtbl;
@@ -160,5 +165,6 @@ Xint32 mce_destroyDeviceAndContext(XIDevice** device, XIContext** ctx) {
 	return 1;
 }
 
+Xconst XIExports* mce_pullSessionsExports() {
 	return g_exp;
 }
--- Minecraft-Community-Edition/Mod/ModLoader.h
+++ Minecraft-Community-Edition/Mod/ModLoader.h
@@ -8,3 +8,4 @@ Xvoid setGlobalQEventBus(mce::core::QEventBus* qBus);
 Xint32 mce_createDeviceAndContext(XIDevice** device, XIContext** ctx);
 Xint32 mce_destroyDeviceAndContext(XIDevice** device, XIContext** ctx);
 
+Xconst XIExports* mce_pullSessionsExports();
