f38cf64681b4e793664cb64a4f514b4d4d4c9bb2
Joud Kandeel <joudkandeel05@gmail.com>
2026-05-20 09:17:56 +0200

    added freetype lib

---
+++ Minecraft-Community-Edition/libs/freetype-2.14.3/_ftall.c
@@ -0,0 +1,39 @@
+#include "src/base/ftsystem.c"
+#include "src/base/ftinit.c"
+#include "src/base/ftdebug.c"
+#include "src/base/ftbase.c"
+
+#include "src/autofit/autofit.c"
+
+#include "src/sfnt/sfnt.c"
+#include "src/truetype/truetype.c"
+
+#include "src/raster/raster.c"
+#include "src/smooth/smooth.c"
+
+#include "src/gzip/ftgzip.c"
+
+#include "src/base/ftbitmap.c"
+#include "src/base/ftglyph.c"
+#include "src/base/ftmm.c"
+
+#include "src/bdf/bdf.c"
+#include "src/pfr/pfr.c"
+
+#include "src/sdf/sdf.c"
+
+#include "src/lzw/ftlzw.c"
+
+#include "src/pcf/pcf.c"
+#include "src/winfonts/winfnt.c"
+
+
+#include "src/type42/t42drivr.c"
+#include "src/type42/t42objs.c"
+#include "src/type42/t42parse.c"
+
+#include "src/svg/ftsvg.c"
+
+#include "src/pshinter/pshinter.c"
+
+#include "src/cid/type1cid.c"
