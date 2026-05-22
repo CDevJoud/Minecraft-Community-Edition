f0f1578d19d67a11fd38f5b2d66ae5222f6bb82b
Joud Kandeel <joudkandeel05@gmail.com>
2026-05-22 22:55:53 +0200

    implemented basic RmlUI into the application

--- Minecraft-Community-Edition/Application.cpp
+++ Minecraft-Community-Edition/Application.cpp
@@ -21,10 +21,28 @@
 #include "TUI/icon.hpp"
 #include "TUI/CLogger.hpp"
 
+#include <RmlUi/Core.h>
+#include <UI/Backend/RmlUI_Platform_dms.hpp>
+#include <UI/Backend/RmlUI_Renderer_bgfx.hpp>
+#include <RmlUi/Core/FontEngineInterface.h>
+
 mce::gfx::Image iImg;
 
 #define LOG_DEBUG(msg) qBus.post(event::Log(event::Log::Severity::DEBUG, msg));
 namespace mce {
+
+	bool RmlProcessEvents(Rml::Context* ctx, sf::Event& event) {
+		/*if (event.type == sf::Event::Resized) {
+			Rml::Vector2i dimension = { event.size.width, event.size.height };
+			ui::priv::RenderInterface_bgfx* ri = reinterpret_cast<ui::priv::RenderInterface_bgfx*>(Rml::GetRenderInterface());
+			ri->setViewport(dimension.x, dimension.y);
+			return true;
+		}
+		else {
+			return ui::priv::inputEventHandler(ctx, nullptr, event);
+		}*/
+	}
+
 	Application::Application(int argc, char* argv[]) :
 		qBus("APP"),
 		threadManager(qBus),
@@ -49,18 +67,14 @@ namespace mce {
 		setupLogging();
 		qBus.runAsync();
 		iImg.loadFromMemory(icon, icon_size);
-		sf::VideoMode s;
 		vfs.buildJSONMappingFile("assets.json", "assets.bin");
 		console.insertComponent(tui::CLogger::createInstance(qBus, "default", 115, 30));
 		console.insertComponent(tui::CLogger::createInstance(qBus, "bgfx", 117, 30));
 		auto component = console.getComponent<tui::CLogger>("default");
 		component->setPosition(119, 1);
+		console.getComponent<tui::CLogger>("bgfx")->setFocus(true);
+		
 		Application::isApplicationInit = Application::initApplication();
-
-		nlohmann::json data = { {"renderCtx", (uint64_t)(&renderCtx)} };
-
-		qBus.post(event::Log(event::Log::Severity::DEBUG, "Hello World!"));
-		LOG_DEBUG("Hello World!");
 	}
 
 	int Application::run() {
@@ -71,7 +85,7 @@ namespace mce {
 
 		int frameCount = 0;
 		int currentFPS = 0;
-
+		int isWindowsNotVisible = 40;
 		bgfx::setDebug(BGFX_DEBUG_STATS);
 		while (true) {
 			{
@@ -95,6 +109,7 @@ namespace mce {
 					}
 				}
 				for (auto& instance : Application::instances) {
+					//std::unique_lock<std::mutex> lock(instanceMutex);
 					for (sf::Event event{}; instance.first->pollEvent(event);) {
 						if (event.type == sf::Event::Closed) {
 							qBus.post(event::window::Close{ instance.first->getSystemHandle() });
@@ -104,9 +119,14 @@ namespace mce {
 						if (event.type == sf::Event::Resized) {
 							qBus.post(event::window::Resize{ instance.first->getSystemHandle(), { event.size.width, event.size.height } });
 						}
+						if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Enter) {
+							ctx->UnloadDocument(doc);
+							doc = ctx->LoadDocument(".\\assets\\ui\\main.html");
+							doc->Show();
+						}
+						ui::priv::inputEventHandler(ctx, instance.first->getSystemHandle(), event);
 					}
 				}
-
 				Application::renderCtx->beginFrame();
 
 				for (auto& instance : Application::instances) {
@@ -114,28 +134,22 @@ namespace mce {
 						instance.second->render();
 					}
 				}
-				
+				ctx->Update();
+				ctx->Render();
+
 				Application::renderCtx->endFrame();
 
 				sf::sleep(sf::milliseconds(0));
 			}
 		}
 		console.close();
-		threadManager.waitAll();
 
-		bool hasBgfxShutdown = false;
-		Thread* th = threadManager.createThread("BgfxShutdown", [this, &hasBgfxShutdown]() {
-			bgfx::frame(BGFX_FRAME_FLUSH | BGFX_FRAME_DISCARD);
-			renderCtx->shutdown();
-			hasBgfxShutdown = true;
-			});
+		Rml::Shutdown();
+		delete rmlSystem;
+		delete rmlRenderer;
 
-		th->launch();
-
-		//Wait for 3 seconds if bgfx refuses to shutdown then we terminates it
-		sf::sleep(sf::seconds(3));
-		if(!hasBgfxShutdown)
-			th->terminate();
+		//bgfx::frame(BGFX_FRAME_FLUSH | BGFX_FRAME_DISCARD);
+		renderCtx->shutdown();
 
 		return 0;
 	}
@@ -197,29 +211,59 @@ namespace mce {
 
 		Application::initQEventBusSubscription();
 
-		renderCtx = eastl::make_shared<gfx::BgfxRenderContext>(qBus);
+		Application::renderCtx = eastl::make_shared<gfx::BgfxRenderContext>(qBus);
 
-		Application::createProfile("MCE:Player1");
+		Application::rsrcMgr = eastl::make_unique<core::ResourceManager>(qBus, vfs, factory, *renderCtx);
+		
+		Application::appWindow = eastl::make_unique<sf::WindowBase>(sf::VideoMode::getDesktopMode(), "Damascene RunTime Engine: v0.2.0");
 
+		if (!Application::isRenderCtxInit) {
+			if (renderCtx->init(*appWindow, api)) {
+				isRenderCtxInit = true;
+			}
+		}
+
+		if (rmlSystem == nullptr)
+			Application::rmlSystem = new ui::priv::SystemInterface_dms(qBus);
+		if (rmlRenderer == nullptr)
+			Application::rmlRenderer = new ui::priv::RenderInterface_bgfx(*rsrcMgr);
+
+		Rml::SetSystemInterface(rmlSystem);
+		Rml::SetRenderInterface(rmlRenderer);
+
+		if (!Rml::Initialise()) {
+			return false;
+		}
+
+		vfs.getFile("assets.fonts.arial_black", fontMem);
+		Rml::Span<const Rml::byte> rmlFontMem(
+			reinterpret_cast<const Rml::byte*>(fontMem.data()),
+			fontMem.size()
+		);
+		
+		Rml::LoadFontFace(rmlFontMem, "arial_black", Rml::Style::FontStyle::Normal);
+		Application::ctx = Rml::CreateContext("main", Rml::Vector2i(1920, 1080));
+
+		doc = Application::ctx->LoadDocument(".\\assets\\ui\\main.html");
+		doc->Show();
+		
+		Application::createProfile("MCE:Player1", eastl::move(appWindow));
+		
 		return true;
 	}
 
 	void Application::initQEventBusSubscription() {
 		
 	}
-	void Application::createProfile(const eastl::string profileName) {
-		eastl::unique_ptr<sf::WindowBase> window = eastl::make_unique<sf::WindowBase>(sf::VideoMode(1920 / 4, 1080 / 2), "Minecraft CE");
-		window->setIcon(iImg.getSize().x, iImg.getSize().y, iImg.getPixelsPtr());
+	void Application::createProfile(const eastl::string profileName, eastl::unique_ptr<sf::WindowBase> window) {
 		uint16_t viewId = 0;
-
-		if (!this->isRenderCtxInit) {
-			if (renderCtx->init(*window, api)) {
-				this->isRenderCtxInit = true;
-			}
-		}
-		else {
+		if (window == nullptr) {
+			window = eastl::make_unique<sf::WindowBase>(sf::VideoMode(1920, 1080), "Minecraft CE");
 			viewId = renderCtx->registerWindow(*window);
 		}
+
+		window->setIcon(iImg.getSize().x, iImg.getSize().y, iImg.getPixelsPtr());
+
 		eastl::unique_ptr<Minecraft> mc = eastl::make_unique<Minecraft>(
 			profileName,
 			Application::qBus,
@@ -230,22 +274,34 @@ namespace mce {
 			Application::factory,
 			Application::vfs
 		);
+
 		Minecraft* rawMinecraftPtr = mc.get();
 		sf::WindowBase* rawWindowPtr = window.get();
 		Application::instances.emplace_back(
 			eastl::make_pair<eastl::unique_ptr<sf::WindowBase>, eastl::unique_ptr<Minecraft>>(eastl::move(window), eastl::move(mc))
 		);
 
+		rmlRenderer->setViewport(rawWindowPtr->getSize().x, rawWindowPtr->getSize().y, 0, 0);
 
-		//create a new thread so we dont block the main thread
+		//create a new thread so we don't block the main thread
 		threadManager.createThread(profileName, [this, rawMinecraftPtr]() {
 			rawMinecraftPtr->run();
 
 			std::lock_guard<std::mutex> lock(instanceMutex);
-
+			
 			auto it = eastl::find_if(instances.begin(), instances.end(), [rawMinecraftPtr](const eastl::pair<eastl::unique_ptr<sf::WindowBase>, eastl::unique_ptr<Minecraft>>& m) {
 				return m.second.get() == rawMinecraftPtr;
 				});
+			
+			// we need the main window that init the bgfx to stay alive until we destroy bgfx
+			// if this window is the master/main window then we pass it to appWindow.
+			// this is a temp fix for now!
+			if (it->first->bIsMaster) {
+				Application::appWindow = eastl::move(it->first);
+			}
+			else {
+				renderCtx->unregisterWindow(it->second->getViewId());
+			}
 
 			if (it != instances.end()) {
 				instances.erase(it);
@@ -258,66 +314,3 @@ namespace mce {
 
 #include <Windows.h>
 MCE_STARTUP(mce::Application);
-
-
-////Trying to find the error LMAO
-//int main() {
-//	sf::WindowBase window(sf::VideoMode(1280, 720), "");
-//	mce::core::QEventBus qBus("APP");
-//	qBus.runAsync();
-//	mce::gfx::BgfxRenderContext ctx(qBus);
-//
-//	mce::io::VirtualFileSystem vfs;
-//	vfs.loadFile("assets");
-//
-//	ctx.init(window, mce::gfx::RenderContext::API::Direct3D11);
-//	bgfx::setDebug(BGFX_DEBUG_STATS);
-//
-//	mce::gfx::RenderFactory factory(qBus);
-//	
-//	mce::gfx::VertexArray vArray;
-//	
-//	vArray.append(mce::gfx::Vertex(sf::Vector3f(1.0f, 1.0f, 1.0f), sf::Color::Red, sf::Vector2f(1.0f, 1.0f)));
-//	auto layout = mce::gfx::Vertex::layout();
-//	
-//	vArray.setVertexLayout(layout);
-//	mce::gfx::flags::Buffer bFlag;
-//	
-//	{
-//		bFlag.addFlag(mce::gfx::flags::Buffer::None);
-//		auto vb = factory.createVertexBuffer(vArray, bFlag, "VertexBuffer");
-//
-//		eastl::vector<uint8_t> vsBytes, fsBytes;
-//		vfs.getFile("assets.shaders.main.vs.d3d11_windows", vsBytes);
-//
-//		vfs.getFile("assets.shaders.main.fs.d3d11_windows", fsBytes);
-//
-//		auto sp = factory.createShaderProgram(eastl::make_pair<eastl::vector<uint8_t>, eastl::vector<uint8_t>>(vsBytes, fsBytes));
-//
-//		bool success = false;
-//		//auto sp = mce::gfx::ShaderProgram(vsBytes, fsBytes, success);
-//
-//		/*const bgfx::Memory* vsMem = bgfx::makeRef(vsBytes.data(), vsBytes.size());
-//		const bgfx::Memory* fsMem = bgfx::makeRef(fsBytes.data(), fsBytes.size());
-//
-//		bgfx::ShaderHandle vs = bgfx::createShader(vsMem);
-//		bgfx::ShaderHandle fs = bgfx::createShader(fsMem);
-//
-//		bgfx::ProgramHandle program = bgfx::createProgram(vs, fs, true);*/
-//
-//		while (window.isOpen()) {
-//			for (sf::Event event; window.pollEvent(event);) {
-//				if (event.type == sf::Event::Closed) {
-//					window.close();
-//				}
-//			}
-//
-//			ctx.beginFrame();
-//
-//			ctx.endFrame();
-//		}
-//		//bgfx::destroy(program);
-//	}
-//
-//	ctx.shutdown();
-//}
